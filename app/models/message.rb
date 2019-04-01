e68bb3a3a12160965d960c49a697970b7a2f63d3
paolozroxas <paolo.z.roxas@gmail.com>
2019-03-31 20:13:53 -0700

    create user and message tables

---
+++ app/models/message.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :integer          not null, primary key
+#  text       :string
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Message < ActiveRecord::Base
+  belongs_to :user
+end
