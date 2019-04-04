95522ab781bc4124697d89f5a781a109660c4589
William <william@Williams-MacBook-Pro.local>
2019-04-03 22:33:59 -0700

    index messages from redisstream

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,9 @@ class MessagesController < ApplicationController
   protect_from_forgery with: :null_session
   
   def index
-    render json: Message.all.sort_by(&:created_at).map(&:serialize)
+    messages = $redis.XREVRANGE("messagestream", "+", "-", "COUNT", 100)
+    parsed_messages = messages.map {|id, message| {user_handle: message[3], text: message[1]}}
+    render json: parsed_messages.to_json
   end
 
   def create
