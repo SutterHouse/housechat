f89bfaf6c869523d39438e7a17e5a5be090aa711
paolozroxas <paolo.z.roxas@gmail.com>
2019-08-17 10:55:19 -0700

    frontend websocket address points to origin

--- frontend/index.js
+++ frontend/index.js
@@ -5,8 +5,8 @@ import App from './components/App';
 import { ActionCableProvider } from 'react-actioncable-provider';
 
 const httpServerUrl = window.location.origin + '/messages';
-
-const AppJsx = <ActionCableProvider url='ws://localhost:3000/cable'>
+const wsUrl = `ws://${window.location.hostname}${window.location.port ? `:${window.location.port}` : ""}/cable`;
+const AppJsx = <ActionCableProvider url={wsUrl}>
   <App httpServerUrl={httpServerUrl} />
 </ActionCableProvider>;
 
