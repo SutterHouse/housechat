97ddba7b7d9ceda9282f1509aac2c1f50914e998
William <william@Williams-MacBook-Pro.local>
2019-08-17 15:55:57 -0700

    Added two @ signs

--- app/controllers/concerns/email_sender.rb
+++ app/controllers/concerns/email_sender.rb
@@ -4,14 +4,14 @@ require 'sendgrid-ruby'
 include SendGrid
 
 module EmailSender
+  @@sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
   def send_email(sender_name, recipient_email, message)
     from = Email.new(email: 'housechatluxe@gmail.com')
     to = Email.new(email: recipient_email)
     subject = "You have a new message from #{sender_name}!"
     content = Content.new(type: 'text/plain', value: message)
     mail = Mail.new(from, subject, to, content)
-    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
+    response = @@sg.client.mail._('send').post(request_body: mail.to_json)
     puts response.status_code
     puts response.body
     puts response.headers
