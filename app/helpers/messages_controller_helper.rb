f6afa0896fe186cd778661a6197ec3ba5dece5f1
paolozroxas <paolo.z.roxas@gmail.com>
2019-08-18 12:32:31 -0700

    banned word

--- app/helpers/messages_controller_helper.rb
+++ app/helpers/messages_controller_helper.rb
@@ -3,7 +3,7 @@ module MessagesControllerHelper
     if message.length < 1 || message.length > 500
       return false
     end
-    filterList = ['dicks are also what I eat', 'I love to eat dick', 'soggy biscuit', '8=>', 'donger', 'bbc']
+    filterList = ['dicks are also what I eat', 'I love to eat dick', 'd i c k', 'soggy biscuit', '8=>', 'donger', 'bbc']
     return filterList.none?{|phrase| message.include?(phrase)}
   end
   def isValidHandle(handle)
